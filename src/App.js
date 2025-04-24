fe153cbb9591578a4880f12f035987a84370caa1
Nayar Joolfoo <nayar@joolfoo.com>
2025-04-24 18:46:31 +0400

    display listcars

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
 import React from "react"
+import ListCars from "./ListCars"
+
+const listCars = [
+  {
+    id: 1,
+    brand: "ford",
+    name: "ford torino",
+    year: "1970-01-01",
+    origin: "USA"
+  },
+  {
+    id: 2,
+    brand: "ford",
+    name: "ford galaxie 500",
+    year: "1970-01-01",
+    origin: "USA"
+  },
+  {
+    id: 3,
+    brand: "chevrolet",
+    name: "chevrolet vega 2300",
+    year: "1971-01-01",
+    origin: "USA"
+  },
+  {
+    id: 4,
+    brand: "peugeot",
+    name: "peugeot 504 (sw)",
+    year: "1972-01-01",
+    origin: "Europe"
+  },
+  {
+    id: 5,
+    brand: "renault",
+    name: "renault 12 (sw)",
+    year: "1972-01-01",
+    origin: "Europe"
+  },
+]
 
 class App extends React.Component {
   render() {
-    return <h1>This is my App.js</h1>
+    return (
+      <div>
+        <h1>This is my App.js</h1>
+        <ListCars/>
+      </div>
+    )
   }
 }
 
