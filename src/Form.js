1f08970d6c5bd6ac61831fa045892e8caf9dd084
Nayar Joolfoo <nayar@joolfoo.com>
2025-04-24 20:53:30 +0400

    react form

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,22 @@
 import InputComponent from "./InputComponent"
 
 function Form(props) {
+    const handlesubmit = function(event) {
+        event.preventDefault();
+        console.log(event.target.getElementsByTagName('input'))
+        for (const item of event.target.getElementsByTagName('input')) {
+            console.log(item.value)
+          }
+
+    }
+
     return (
-    <form>
+    <form onSubmit={handlesubmit}>
         <InputComponent name="name" type="text" label="Name"></InputComponent>
         <InputComponent name="age" type="number" label="Age"></InputComponent>
         <InputComponent name="email" type="email" label="Email"></InputComponent>
+        <InputComponent name="address" type="text" label="Address"></InputComponent>
+        <InputComponent name="phone" type="text" label="Phone"></InputComponent>
         <button type="submit">Submit</button>
     </form>)
 }
